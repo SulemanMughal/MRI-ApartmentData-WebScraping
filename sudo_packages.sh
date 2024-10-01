ace072803be03b2b194eb5f3a1ea727f7a169706
observersilent6 <observersilent6@gmail.com>
2024-10-01 08:43:30 +0500

    Asd32424

--- sudo_packages.sh
+++ sudo_packages.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
 sudo apt-get install xvfb
+
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+
+sudo apt --fix-broken install
+
+
+sudo apt install google-chrome-stable
--- test_selenium.py
+++ test_selenium.py
@@ -2,6 +2,15 @@ import chromedriver_autoinstaller
 from pyvirtualdisplay import Display
 from selenium import webdriver
 
+from selenium.webdriver.chrome.options import Options
+
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-gpu")
+chrome_options.add_argument("--no-sandbox")  # Required when running as root
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--remote-debugging-port=9222")
+
 # Start a virtual display
 with Display(visible=0, size=[800, 600]) as display:
     
@@ -9,7 +18,7 @@ with Display(visible=0, size=[800, 600]) as display:
     chromedriver_autoinstaller.install()
 
     # Create a Selenium WebDriver with the virtual display
-    driver = webdriver.Chrome()
+    driver = webdriver.Chrome(options=chrome_options)
 
     # Now you can use the WebDriver to interact with a web page
     driver.get("https://www.google.com")
